b5c164b8ab0e3b40adcf72d689becb9c8978348f
Stéphane PY <py.stephane1@gmail.com>
2012-09-19 10:40:10 +0200

    Works in standalone or with formulate + hoa\math as sub deps

--- src/Rezzza/Formulate/Evaluer/Evaluer.php
+++ src/Rezzza/Formulate/Evaluer/Evaluer.php
@@ -9,7 +9,46 @@ namespace Rezzza\Formulate\Evaluer;
  */
 class Evaluer
 {
-    private static $evaluer;
+    /**
+     * @var \Hoa\Compiler\Llk
+     */
+    private static $compiler;
+
+    /**
+     * @var \Hoa\Math\Visitor\Arithmetic
+     */
+    private static $visitor;
+
+    /**
+     * @return \Hoa\Compiler\Llk
+     */
+    static public function getCompiler()
+    {
+        if (!self::$compiler) {
+            from('Hoa')
+                ->import('Compiler.Llk')
+                ->import('File.Read')
+                ;
+
+            // cannot be used actually, issue with composer
+            //$grammar = 'hoa://Library/Math/Arithmetic/Grammar.pp';
+            $grammar = realpath(__DIR__.'/../../../../../../hoa/math/Arithmetic/Grammar.pp');
+
+            if (!$grammar) {
+                $grammar = realpath(__DIR__.'/../../../../vendor/hoa/math/Arithmetic/Grammar.pp');
+
+                if (!$grammar) {
+                    throw new \Exception('Path to \Hoa\Math\Arithmetic\Grammar is corrupted');
+                }
+            }
+
+            static::$compiler = \Hoa\Compiler\Llk::load(
+                new \Hoa\File\Read($grammar)
+            );
+        }
+
+        return static::$compiler;
+    }
 
     /**
      * @return \Hoa\Math\Evaluer\Evaluer
